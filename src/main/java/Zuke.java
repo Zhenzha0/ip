7c2b0ca16a62e2b44e133778e0142d2a9e6c7081
Yang Zhenzhao <robertyzz02@gmail.com>
2025-08-24 21:40:06 +0800

    level 0

---
+++ src/main/java/Zuke.java
@@ -0,0 +1,19 @@
+public class Zuke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|_   / _   _| |  ____ \n"
+                + "  / / | | | | |/ / _ \\\n"
+                + " / /_ | |_| |   <  __/\n"
+                + "|____| \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
+        printline();
+        System.out.println("Hello! I'm Zuke\n" + "What can I do for you?");
+        printline();
+        System.out.println("Bye. Hope to see you again soon!");
+        printline();
+    }
+
+    public static void printline() {
+        System.out.println("____________________________________________________________");
+    }
+}
